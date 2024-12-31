9c3b65eb8f8ee2bf3bf186116460f715c18f3c33
Muaaz Raza <yourssharky@gmail.com>
2024-12-31 13:36:26 +0500

    add next-sitemap to improve seo

---
+++ apps/frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://resourcle.vercel.app', // Your website URL
+    generateRobotsTxt: true, // Optionally generate robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+};
