6ed4f5761b8ad0776f28c3786e5107bdf375e0f4
Muaaz Raza <yourssharky@gmail.com>
2024-12-31 15:18:49 +0500

    adjust util-server index file

--- apps/util-server/src/server.ts
+++ apps/util-server/src/server.ts
@@ -1,8 +1,8 @@
 import Fastify from 'fastify';
 import utilsRoutes from './routes/util.routes.js';
-import cors, { FastifyCorsOptions } from '@fastify/cors'; 
+import cors from '@fastify/cors'
 const fastify = Fastify();
-const corsOptions: FastifyCorsOptions = {
+const corsOptions = {
   origin: ["https://resourcle.vercel.app","http://localhost:3000"], // Allows all origins. You can specify an array or function for specific origins
   credentials: true, // Allow cookies to be sent with requests (if needed)
   maxAge: 86400 // Cache preflight request results for 24 hours
@@ -11,14 +11,15 @@ fastify.register(cors, corsOptions);
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' };
+  return { message: 'Hello world all the way from the Mars' };
 });
 
 fastify.register(utilsRoutes , { prefix: '/api/utils' });
 // Run the server
+const port = +(process.env.PORT||4000) || 4000
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT||4000 });
+    await fastify.listen({ port,host:"0.0.0.0"});
     console.log('Util Server listening at http://localhost:4000');
   } catch (err) {
     console.error(err);
