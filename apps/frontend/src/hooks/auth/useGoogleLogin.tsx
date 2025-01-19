2ed72087b8413c69ca69e9ac734354b909b6a5df
Muaaz Raza <yourssharky@gmail.com>
2025-01-20 01:11:46 +0500

    force cookie set on frontend

--- apps/frontend/src/hooks/auth/useGoogleLogin.tsx
+++ apps/frontend/src/hooks/auth/useGoogleLogin.tsx
@@ -5,11 +5,13 @@ import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
 import { useMutation } from 'react-query'
 import { useSetRecoilState } from 'recoil'
+import Cookie from "js-cookie"
 export default function useGoogleLogin() {
     const router = useRouter()
   const setState = useSetRecoilState(authAtom)
   return (
     useMutation({mutationFn:(credential:string)=>LoginWithGoogle(credential),onSuccess(data) {
+      Cookie.set(process.env.NEXT_PUBLIC_SESSION_COOKIE_KEY,data.token,{expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),})
         setState(e=>({...e,isLogined:true,user:data.payload,authReminderModal:false}))
         toast.success("Logged in successfully")
         router.push("/")
--- apps/frontend/src/hooks/useLoginLocal.tsx
+++ apps/frontend/src/hooks/useLoginLocal.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { useMutation } from "react-query";
 import { useSetRecoilState } from "recoil";
+import Cookie from "js-cookie"
 const useLoginLocal = () => {
     const router = useRouter()
     const setState = useSetRecoilState(authAtom)
@@ -12,6 +13,8 @@ const useLoginLocal = () => {
       mutationFn: (payload:{email:string,password:string}) => LoginLocal(payload),
       onSuccess(data) {
         setState(e=>({...e,isLogined:true,user:data.payload,authReminderModal:false}))
+          // Set session cookie just becuase of domain confilct
+          Cookie.set(process.env.NEXT_PUBLIC_SESSION_COOKIE_KEY,data.token,{expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),})
         toast.success(data.message)
         router.push("/")
       },
