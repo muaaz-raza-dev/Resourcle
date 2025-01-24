c74f5c912f87525cb4d15b1e0eb07654dc979bf3
Muaaz Raza <yourssharky@gmail.com>
2025-01-24 22:57:38 +0500

    add robot.txt for indexing

--- apps/frontend/src/app/robots.ts
+++ apps/frontend/src/app/robots.ts
@@ -6,7 +6,10 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: ['/',"/auth/login","/auth/signup"],
       disallow: ['/auth/',"/settings","/collection"],
+      crawlDelay:10,
+      
     },
+    host: process.env.NEXT_PUBLIC_URL,
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
   }
 }
