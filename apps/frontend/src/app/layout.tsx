8fa0cd039ff36b39f98c0f79a962b7d4fdad7cab
Muaaz Raza <yourssharky@gmail.com>
2025-02-10 16:54:09 +0500

    attach google analytics

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import dynamic from "next/dynamic";
 import { Toaster } from 'react-hot-toast';
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import Head from "next/head";
+import Script from "next/script"
 import { Rootmetadata } from "@/assets/root/root-meta";
 import { RootSchemaOrgData } from "@/assets/root/root-schema-org";
 
@@ -14,7 +14,7 @@ const RecoilProvider = dynamic(() => import('@/components/providers/recoil-provi
 const QueryClientsProvider = dynamic(() => import('@/components/providers/query-client-provider'), { ssr: false });
 const AuthReminderModal = dynamic(() => import('@/components/global/auth-reminder-modal'), { ssr: false });
 const AuthUiValidator = dynamic(() => import('@/components/validators/auth-ui-validator'), { ssr: false });
-
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
 export const metadata=Rootmetadata;
 
 
@@ -25,17 +25,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"  />
         <link rel="canonical" href="https://resourcle.com" />
-        <script
+      
+
+        <meta name="google-site-verification" content="coRJi_s_5EGqv50oNGEGIIij14T0DrAtA9rYfELy0Kc" />
+        <Script 
+          id="g-key"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(RootSchemaOrgData),
           }}
         />
-        <meta name="google-site-verification" content="coRJi_s_5EGqv50oNGEGIIij14T0DrAtA9rYfELy0Kc" />
-      </Head>
+
+<Script  strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
+<Script id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}');
+                `,
+              }}
+            />
+      </head>
       <body className={`antialiased`}>
         <Toaster />
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
